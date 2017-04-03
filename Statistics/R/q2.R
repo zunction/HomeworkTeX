4a2f11e00a67cb08280e3b3803c08d13088a5fde
Zhangsheng Lai <dunno.noe@gmail.com>
2017-04-03 08:36:22 +0800

    adding R stuff

--- Statistics/R/q2.R
+++ Statistics/R/q2.R
@@ -1,10 +1,23 @@
 library(leaps)
+library(dplyr)
 
+# read csv file into dataframe car
 car <- read.csv('carmpgdat.csv')
 
-lm.1 <- lm(MPG~VOL+HP+SP+WT, data = car)
+# generate a linear model with normally distributed noise with the model MPG~VOL+HP+SP+WT
+lm <- lm(MPG~VOL+HP+SP+WT, data = car)
 
-leaps(x = car[,c('VOL','HP','SP','WT')], y = car[,'MPG'], names =c('VOL','HP','SP','WT'))
+# estimated regression function and residual sum of squares
+print(lm$coefficients)
+
+# MPG = 192.43775332 - 0.01564501 * VOL + 0.39221231 * HP - 1.29481848 * SP - 1.85980373 * WT
+RSS = sum(lm$residuals^2)
+print(RSS)
+
+
+
+
+leaps(x = car[,c('VOL','HP','SP','WT')], y = car[,'MPG'], names =c('VOL','HP','SP','WT'), method = 'Cp')
 
 
 
