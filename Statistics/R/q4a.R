11e6adadaef840b2e5f1b6f8ead1b06ef3985d8d
Zhangsheng Lai <dunno.noe@gmail.com>
2017-04-04 10:35:45 +0800

    done with stats HW!!

--- Statistics/R/q4a.R
+++ Statistics/R/q4a.R
@@ -14,7 +14,7 @@ se_mc <- sqrt(var(sapply(1:N,I)))
 # Estimated standard error using 10000 bootstrap samples.
 B <- 10000
 base_sample <- rgamma(1.5,1/2.3,n = N)
-I_bootstrap <- function(X){X = sample(base_sample, N, replace = TRUE);return(sum(1<X & X<2)/N)}
+I_bootstrap <- function(u){X = sample(base_sample, N, replace = TRUE);return(sum(1<X & X<2)/N)}
 se_bootstrap <- sqrt(var(sapply(1:B,I_bootstrap)))
 
 # se_bootstrap = 0.001273952
