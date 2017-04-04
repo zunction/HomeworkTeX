53becbd5b385ec052bfe6c7de54b069e5a510084
Zhangsheng Lai <dunno.noe@gmail.com>
2017-04-04 10:11:48 +0800

    I think I"m done with HW4

--- Statistics/R/q2.R
+++ Statistics/R/q2.R
@@ -20,13 +20,85 @@ print(lm_car$coefficients)
 RSS = sum(lm_car$residuals^2)
 print(RSS)
 
-# (b) Mallow Cp
+# (b) Mallows's Cp
+# Since the AIC criterion is equivalent ot Mallows's Cp, 
 
 # (i) Forward
+base <- lm(MPG~WT, data = car)
+step(base, scope = list(upper = lm_car, lower=~1), direction = 'forward', trace = TRUE)
+
+# Start:  AIC=240.45
+# MPG ~ WT
+# 
+# Df Sum of Sq    RSS    AIC
+# + SP    1    82.981 1383.0 237.68
+# + HP    1    35.380 1430.6 240.45
+# <none>              1466.0 240.45
+# + VOL   1     3.883 1462.1 242.24
+# 
+# Step:  AIC=237.68
+# MPG ~ WT + SP
+# 
+# Df Sum of Sq    RSS    AIC
+# + HP    1    349.37 1033.7 215.80
+# + VOL   1     45.97 1337.0 236.90
+# <none>              1383.0 237.68
+# 
+# Step:  AIC=215.8
+# MPG ~ WT + SP + HP
+# 
+# Df Sum of Sq    RSS   AIC
+# <none>              1033.7 215.8
+# + VOL   1    6.2685 1027.4 217.3
+# 
+# Call:
+#   lm(formula = MPG ~ WT + SP + HP, data = car)
+# 
+# Coefficients:
+#   (Intercept)           WT           SP           HP  
+# 194.1296      -1.9221      -1.3200       0.4052
 
 # (ii) Backward
+step(lm_car,direction = 'backward', trace = TRUE)
 
-# leaps(x = car[,c('VOL','HP','SP','WT')], y = car[,'MPG'], names =c('VOL','HP','SP','WT'), method = 'Cp')
+# Start:  AIC=217.3
+# MPG ~ VOL + HP + SP + WT
+# 
+# Df Sum of Sq    RSS    AIC
+# - VOL   1      6.27 1033.7 215.80
+# <none>              1027.4 217.30
+# - HP    1    309.67 1337.0 236.90
+# - SP    1    373.36 1400.7 240.72
+# - WT    1   1013.76 2041.2 271.59
+# 
+# Step:  AIC=215.8
+# MPG ~ HP + SP + WT
+# 
+# Df Sum of Sq    RSS    AIC
+# <none>              1033.7 215.80
+# - HP    1    349.37 1383.0 237.68
+# - SP    1    396.97 1430.6 240.45
+# - WT    1   1322.87 2356.5 281.37
+# 
+# Call:
+#   lm(formula = MPG ~ HP + SP + WT, data = car)
+# 
+# Coefficients:
+#   (Intercept)           HP           SP           WT  
+# 194.1296       0.4052      -1.3200      -1.9221  
+# 
+# For the forward stepwise approach, the base model is important
+# as it will change the outcome. For example for this, if we were
+# to start with VOL instead of any of the other covariates, we 
+# will end up with also the VOL covariate. By not starting with with
+# the VOL covariate we will end up with a model without VOL which 
+# corresponds to the backward stepwise approach and also the 
+# Zheng-Loh model selection. As for the backward stepwise approach
+# we do not have such a problem as we start the model with all the 
+# covariates and reduce it down by computing the AIC of the model
+# with different covariate missing then remove the covariate that 
+# gives the smallest AIC when removed.
+
 
 
 # (c) Zheng-Loh Model Selection
@@ -55,23 +127,33 @@ regTermTest(lm_car, 'WT', null=NULL,df=Inf, method = "Wald")
 # 
 # Arranging in descending order we have:
 #   WT > SP > HP > VOL
+# We can do this as the Chisq test statistic is just the square 
+# of the test statistic of the Wald test.
 # 
 # Let lm_j be the linear model with the jth largest Wald test statistic
 
+n <- nrow(car)
+
 lm_1 <- lm(MPG ~ WT, data = car)
-jhat_1 = sum(lm_1$residuals^2) + 1 * RSS * log(82) 
+jhat_1 = sum(lm_1$residuals^2) + (1 * RSS/(n-4) * log(n))
 print (jhat_1)
+# jhat_1 = 1524.045
 
 lm_2 <- lm(MPG ~ WT + SP, data = car)
-jhat_2 = sum(lm_2$residuals^2) + 2 * RSS * log(82) 
+jhat_2 = sum(lm_2$residuals^2) + (2 * RSS/(n-4) * log(n))
 print (jhat_2)
+# jhat_2 = 1499.108
 
 lm_3 <- lm(MPG ~ WT + SP + HP, data = car)
-jhat_3 = sum(lm_3$residuals^2) + 3 * RSS * log(82) 
+jhat_3 = sum(lm_3$residuals^2) + (3 * RSS/(n-4) * log(n)) 
 print (jhat_3)
+# jhat_3 =  1207.78
 
-jhat_4 = RSS + 4 * RSS * log(82) 
+jhat_4 = RSS + (4 * RSS/(n-4) * log(n)) 
 print (jhat_4)
+# jhat_4 = 1259.555
 
-
-print(RSS)
+# Thus the Zheng-Loh model selection method will select WT, SP and HP 
+# as the covariates for predicting the MPG. Comparing it to (b), we see
+# that the Zheng-Loh model selection method gives similar outcome to using
+# Mallows's Cp model forward and backward stepwise to selecting a model.
